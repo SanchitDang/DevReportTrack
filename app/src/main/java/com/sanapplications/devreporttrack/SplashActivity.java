8fb4cdd0736572736ca2b41dd7cadaf1bd647b95
Sanchit <sanchit.dang03@gmail.com>
2023-05-11 20:37:58 +0530

    added isLoggedin and splash screen

---
+++ app/src/main/java/com/sanapplications/devreporttrack/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.sanapplications.devreporttrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(() -> {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null){
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }else{
+                startActivity(new Intent(this, sendotpActivivity.class));
+                finish();
+            }
+        },2000);
+
+    }
+
+//    @Override
+//    protected  void onStart(){
+//        super.onStart();
+//
+//        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+//
+//        if(firebaseUser!=null){
+//            startActivity(new Intent(this, MainActivity.class));
+//            finish();
+//        }else{
+//            startActivity(new Intent(this, sendotpActivivity.class));
+//            finish();
+//        }
+//
+//    }
+
+}
